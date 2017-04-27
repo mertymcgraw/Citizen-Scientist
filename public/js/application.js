b58e493043475c03a84e5cbe1c2c5367f7a305b3
mertymcgraw <email": null>
2017-04-26 20:37:42 -0700

    Add fade in/out to logbook entries

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  displayFullDiveDetials();
 });
+
+var displayFullDiveDetials = function(){
+    $('.dive_entry_details').mouseover(
+    function () {
+      console.log($(this))
+
+
+      $(this).find(".secondary_dive_details").fadeIn();
+    });
+
+    $('.dive_entry_details').mouseleave(function(){
+      $(this).find(".secondary_dive_details").fadeOut();
+    });
+}
